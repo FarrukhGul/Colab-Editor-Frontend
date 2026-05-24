7b9cc801b8b365387c79484bae3ab2557d1921be
FarrukhGul <coder.gulfarrukh@gmail.com>
2026-05-24 20:57:33 +0500

    fixed

--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-export const Loader = () => {
-  return (
-    <div className="flex items-center justify-center h-screen bg-[#0a0a0a]">
-      <div className="relative w-12 h-12">
-        <div className="absolute inset-0 border-4 border-green-400 border-t-transparent rounded-full animate-spin"></div>
-      </div>
-    </div>
-  )
-}
--- src/components/Routes/ProtectedRoutes.jsx
+++ src/components/Routes/ProtectedRoutes.jsx
@@ -1,13 +1,19 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom"
 import { AuthContext } from "../../context/AuthContext"
-import { Loader } from "../components/Loader"
+
 
 
 export const ProtectedRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext)
 
-if (loading) return <Loader />
+ if (loading) {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-6 h-6 border-2 border-green-400 border-t-transparent rounded-full animate-spin" />
+        </div>
+    )
+}
 
     if(!user) return <Navigate to="/login" replace />
 
