2cc7a4124041bd75126b9ddf7b543a3d3bef738f
FarrukhGul <coder.gulfarrukh@gmail.com>
2026-05-24 20:52:33 +0500

    fix auth loading flicker and mobile black screen issue

--- src/components/Routes/ProtectedRoutes.jsx
+++ src/components/Routes/ProtectedRoutes.jsx
@@ -1,18 +1,13 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom"
 import { AuthContext } from "../../context/AuthContext"
+import { Loader } from "../components/Loader"
 
 
 export const ProtectedRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext)
 
- if (loading) {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="w-6 h-6 border-2 border-green-400 border-t-transparent rounded-full animate-spin" />
-        </div>
-    )
-}
+if (loading) return <Loader />
 
     if(!user) return <Navigate to="/login" replace />
 
